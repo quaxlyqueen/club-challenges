f9cfe8fbd2d728626ed1fc6af398c03b2ed94a7d
Abbas Al-younis <abbasal-younis@Abbass-MacBook-Air.local>
2023-11-02 17:33:20 -0600

    Implemented the sort by title functionality

--- Task.java
+++ Task.java
@@ -1,48 +1,83 @@
+
+import java.util.Arrays;
+import java.util.Comparator;
+
 // Represents a Task, which can be written and loaded to/from a file and are comparable to each other.
 public class Task {
-    private String title;
-    private String description;
-    private String dueDate;
-    private boolean completed;
+	private String title;
+	private String description;
+	private String dueDate;
+	private boolean completed;
 
-    // Constructor to create a new Task object.
-    public Task(String title, String description, String dueDate) {
-        this.title = title;
-        this.description = description;
-        this.dueDate = dueDate;
-        this.completed = false;
-    }
+	public static final Comparator<Task> BY_TITLE = new SortByTitleComparator();
 
-    public String getTitle() {
-        return title;
-    }
+	// Constructor to create a new Task object.
+	public Task(String title, String description, String dueDate) {
+		this.title = title;
+		this.description = description;
+		this.dueDate = dueDate;
+		this.completed = false;
+	}
 
-    public String getDescription() {
-        return description;
-    }
+	public String getTitle() {
+		return title;
+	}
 
-    public String getDueDate() {
-        return dueDate;
-    }
+	public String getDescription() {
+		return description;
+	}
 
-    public boolean isCompleted() {
-        return completed;
-    }
+	public String getDueDate() {
+		return dueDate;
+	}
 
-    // Edit a task by providing new information.
-    public void editTask(String newTitle, String newDescription, String newDueDate) {
-        this.title = newTitle;
-        this.description = newDescription;
-        this.dueDate = newDueDate;
-    }
+	public boolean isCompleted() {
+		return completed;
+	}
 
-    // Return if a task is complete or not.
-    public void toggleComplete() {
-        if(completed) completed = false;
-        else completed = true;
+	// Edit a task by providing new information.
+	public void editTask(String newTitle, String newDescription, String newDueDate) {
+		this.title = newTitle;
+		this.description = newDescription;
+		this.dueDate = newDueDate;
+	}
 
-        System.out.println("task status toggled");
-    }
+	// Return if a task is complete or not.
+	public void toggleComplete() {
+		if (completed)
+			completed = false;
+		else
+			completed = true;
 
-    // TODO: Implement Comparable or Comparator.
+		System.out.println("task status toggled");
+	}
+
+	// TODO: Implement Comparable or Comparator.
+	private static class SortByTitleComparator implements Comparator<Task> {
+
+		@Override
+		public int compare(Task t1, Task t2) {
+			String[] tokens1 = t1.title.split("(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)");
+			String[] tokens2 = t2.title.split("(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)");
+
+			int length = Math.min(tokens1.length, tokens2.length);
+
+			for (int i = 0; i < length; i++) {
+				if (Character.isLetter(tokens1[i].charAt(0)) && Character.isLetter(tokens2[i].charAt(0))) {
+					int stringCompare = tokens1[i].compareTo(tokens2[i]);
+					
+					if (stringCompare != 0) 
+						return stringCompare;
+				} else {
+					int intCompare = Integer.compare(Integer.parseInt(tokens1[i]), Integer.parseInt(tokens2[i]));
+					
+					if (intCompare != 0) 
+						return intCompare;
+				}
+			}
+
+			return Integer.compare(tokens1.length, tokens2.length);
+		}
+
+	}
 }
